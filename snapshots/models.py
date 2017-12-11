09c54eee852b3d185c4d735093cd2e33ec349d92
Zdenek Hynek <zdenek@signal-noise.co.uk>
2017-12-11 05:46:15 +0000

    Add ability to post images

--- snapshots/models.py
+++ snapshots/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Snapshot(models.Model):
     """This class represents the snaphost model."""
     url = models.CharField(max_length=255, blank=False)
+    title = models.CharField(max_length=1000, blank=False)
     source_code = models.TextField()
-    image_url = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(upload_to='uploads', max_length=254, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- snapshots/serializers.py
+++ snapshots/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
+
 from .models import Snapshot
 
 
 class SnapshotSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
+    image = Base64ImageField()
 
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = Snapshot
-        fields = ('id', 'url', 'source_code', 'image_url', 'created_at',
+        fields = ('id', 'url', 'source_code', 'image', 'created_at',
                   'updated_at')
         read_only_fields = ('created_at', 'updated_at')
