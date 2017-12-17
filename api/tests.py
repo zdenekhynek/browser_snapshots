68f49c6e0feadf2d4b3dcaeb6c84682f0f6b2f12
Zdenek Hynek <zdenek@signal-noise.co.uk>
2017-12-17 10:05:07 +0000

    Fix tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,5 @@
+import base64
+
 from django.test import TestCase
 
 from rest_framework.test import APIClient
@@ -13,8 +15,10 @@ class ViewTestCase(TestCase):
     def setUp(self):
         """Define the test client and other test variables."""
         self.client = APIClient()
-        self.snaphost_data = {'url': 'Url', 'source_code': 'Source code',
-                              'image_url': 'image_url'}
+        image = base64.b64encode(b'')   # mock base64 image
+        self.snaphost_data = {'agent': 0, 'url': 'Url',
+                              'source_code': 'Source code',
+                              'image': image}
         self.response = self.client.post(
             reverse('create'),
             self.snaphost_data,
@@ -38,7 +42,7 @@ class ViewTestCase(TestCase):
         """Test the api can update a given snapshot."""
         snapshot = Snapshot.objects.get()
         change_snapshot = {'url': 'Url'}
-        res = self.client.put(
+        res = self.client.patch(
             reverse('details', kwargs={'pk': snapshot.id}),
             change_snapshot, format='json'
         )
--- snapshots/tests.py
+++ snapshots/tests.py
@@ -1,3 +1,5 @@
+import base64
+
 from django.test import TestCase
 
 from .models import Snapshot
@@ -9,7 +11,12 @@ class ModelTestCase(TestCase):
     def setUp(self):
         """Define the test client and other test variables."""
         self.url = "Snaphost url"
-        self.snapshot = Snapshot(url=self.url)
+        self.agent_id = 0
+        self.source_code = 'source_code'
+        self.image = base64.b64encode(b'')   # mock base64 image
+        self.snapshot = Snapshot(url=self.url, agent_id=self.agent_id,
+                                 source_code=self.source_code,
+                                 image=self.image)
 
     def test_model_can_create_a_snapshot(self):
         """Test the snapshot model can create a snapshot."""
