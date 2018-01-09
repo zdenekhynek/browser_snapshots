0877af0b1d2aed76535e4b71f7a0443ab43772c3
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-01-09 18:11:22 +0000

    Make sure heroku static files work as expected

--- browser_snapshots/settings/staging.py
+++ browser_snapshots/settings/staging.py
@@ -8,3 +8,17 @@ ALLOWED_HOSTS = [
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Make sure static assets work on heroku
+# https://devcenter.heroku.com/articles/django-assets
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
