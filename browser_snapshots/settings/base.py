3b6810f3849a60b9cb12c8223a6a9b1bd35f0d41
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-02-06 06:18:37 +0000

    Basic pagination

--- browser_snapshots/settings/base.py
+++ browser_snapshots/settings/base.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'browser_snapshots.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(ROOT_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- snapshots/admin.py
+++ snapshots/admin.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.contrib.admin.views.main import ChangeList
+from django.core.paginator import EmptyPage, InvalidPage, Paginator
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from django.urls import reverse
@@ -20,6 +22,19 @@ class SnapshotAdmin(admin.ModelAdmin):
     list_filter = ('agent', 'owner')
 
 
+class InlineChangeList(object):
+    can_show_all = True
+    multi_page = True
+    get_query_string = ChangeList.__dict__['get_query_string']
+
+    def __init__(self, request, page_num, paginator):
+        self.show_all = 'all' in request.GET
+        self.page_num = page_num
+        self.paginator = paginator
+        self.result_count = paginator.count
+        self.params = dict(request.GET.items())
+
+
 class SnaphostInline(admin.StackedInline):
     def thumbnail_tag(self, obj):
         image_url = obj.image.url
@@ -37,6 +52,50 @@ class SnaphostInline(admin.StackedInline):
     fields = ('url', 'title', 'thumbnail_tag', 'link_tag', 'created_at')
     readonly_fields = ('created_at', 'thumbnail_tag', 'link_tag')
 
+    extra = 0
+    per_page = 2
+    template = 'admin/edit_inline/stacked.html'
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset_class = super(SnaphostInline, self).get_formset(
+            request, obj, **kwargs)
+
+        class PaginationFormSet(formset_class):
+            def __init__(self, *args, **kwargs):
+                super(PaginationFormSet, self).__init__(*args, **kwargs)
+
+                qs = self.queryset
+                paginator = Paginator(qs, self.per_page)
+
+                try:
+                    page_num = int(request.GET.get('p', '0'))
+                except ValueError:
+                    page_num = 0
+
+                try:
+                    page = paginator.page(page_num + 1)
+                except (EmptyPage, InvalidPage):
+                    page = paginator.page(paginator.num_pages)
+
+                self.cl = InlineChangeList(request, page_num, paginator)
+                self.paginator = paginator
+
+                if self.cl.show_all:
+                    self._queryset = qs
+                else:
+                    self._queryset = page.object_list
+
+                self.page_num = page_num + 1
+
+                self.has_previous = self.page_num > 1
+                self.has_next = self.page_num < paginator.num_pages
+
+                self.previous_page_number = page_num - 1
+                self.next_page_number = page_num + 1
+
+        PaginationFormSet.per_page = self.per_page
+        return PaginationFormSet
+
 
 class SessionAdmin(admin.ModelAdmin):
     fields = ('owner', 'agent', 'start', 'end', 'lat',
@@ -46,6 +105,14 @@ class SessionAdmin(admin.ModelAdmin):
     inlines = [
         SnaphostInline,
     ]
+
+    list_display = ['start', 'agent', 'owner']
+
+    # def get_name(self, obj):
+    #     return obj.author.name
+
+    # get_name.admin_order_field  = 'author'  #Allows column order sorting
+    # get_name.short_description = 'Author Name'  #Renames column head
 
 
 # Register your models here.
