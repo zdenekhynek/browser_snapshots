a8d44351ca3790190e8c462d1bfd9abdb93bf031
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-03-14 09:30:06 +0000

    Parsed watson

--- analysis/admin.py
+++ analysis/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from analysis.models import Sentiment
+from analysis.watson_tone_analyzer import parse_raw_tone
 
-admin.site.register(Sentiment)
+class SentimentAdmin(admin.ModelAdmin):
+    def parsed_tag(self, obj):
+        parsed_json = parse_raw_tone(obj.watson_raw_tone)
+        tones = parsed_json['document_tone']['tones']
+        lis = ['<li>%s: %s</li>' % (tone['tone_name'], tone['score'])
+          for tone in tones]
+        lis_string = ''.join(lis)
+
+        return mark_safe('<ul style="margin-left:10px;">%s</ul>' % lis_string)
+
+    parsed_tag.short_description = 'Watson tone'
+
+    fields = ('title', 'sentiment', 'parsed_tag')
+    readonly_fields = ('parsed_tag',)
+
+admin.site.register(Sentiment, SentimentAdmin)
--- analysis/watson_tone_analyzer.py
+++ analysis/watson_tone_analyzer.py
@@ -6,6 +6,10 @@ from django.conf import settings
 from watson_developer_cloud import ToneAnalyzerV3
 
 
+def parse_raw_tone(raw_tone_json):
+        return json.loads(raw_tone_json)
+
+
 class WatsonToneAnalyzer():
     def __init__(self):
         username = settings.WATSON_USERNAME
@@ -21,3 +25,5 @@ class WatsonToneAnalyzer():
     def tone(self, tone_input):
         return self.tone_analyzer.tone(tone_input=tone_input,
                                        content_type='text/plain')
+
+
