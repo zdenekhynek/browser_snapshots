3c8536be6ea0ebf87efca3b20fce821267be6a2b
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-03-30 16:18:33 +0100

    Serialize config

--- scenarios/serializers.py
+++ scenarios/serializers.py
@@ -2,13 +2,23 @@ from rest_framework import serializers
 
 from .models import Scenario, ScenarioConfig
 
+
+class ConfigSerializer(serializers.Field):
+    def to_representation(self, obj):
+        ret = {
+            obj.key: obj.value
+        }
+        return ret
+
+
 class ScenarioConfigSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
+    settings = ConfigSerializer(source='*')
 
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = ScenarioConfig
-        fields = ('key', 'value')
+        fields = ('settings',)
 
 
 class ScenarioSerializer(serializers.ModelSerializer):
