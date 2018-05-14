053470540a731d6487084d5dfecb0fcfbc3e25cb
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-05-14 16:29:29 +0100

    Add chart

--- frontend/src/races/chart.jsx
+++ frontend/src/races/chart.jsx
@@ -25,19 +25,84 @@ export const COLORS = [
 export const MARGIN = { top: 20, right: 20, bottom: 30, left: 40 };
 
 class Chart extends Component {
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const { tasks, size } = nextProps;
+    const { width } = size;
+
+    const data = tasks.reduce((acc, t, i) => {
+      const newT = t.map((d) => {
+        return d.set('index', i);
+      });
+      acc.push(newT.toJS());
+      return acc;
+    }, []);
+
+    const chartWidth = width - MARGIN.left - MARGIN.right;
+    const chartHeight = 600 - MARGIN.top - MARGIN.bottom;
+
+    // setup x
+    const xValue = (d, i) => i;
+    const xScale = scaleLinear().range([0, chartWidth]);
+    const xMap = (d, i) => xScale(xValue(d, i));
+
+    // setup y
+    const yValue = (d) => {
+      return (d.temperature && !Number.isNaN(d.temperature)) ? d.temperature : 0;
+    };
+    const yScale = scaleLinear().range([chartHeight, 0]); // value -> display
+    const yMap = (d) => yScale(yValue(d));
+
+    const sizeValue = (d) => d.views;
+    const sizeScale = scaleLinear().range([2, 20]);
+    const sizeMap = (d) => sizeScale(sizeValue(d));
+
+    let colorIndex = -1;
+    const colorMap = (d) => {
+      if (typeof d.index !== 'undefined') {
+        return COLORS[d.index];
+      }
+
+      colorIndex += 1;
+      return COLORS[colorIndex];
+    };
+
+    // don't want dots overlapping axis, so add in buffer to data domain
+    const flattenedData = data.reduce((acc, d) => {
+      return acc.concat(d);
+    }, []);
+    const dataLens = data.map((d) => {
+      return d.length;
+    });
+
+    xScale.domain([-1, max(dataLens) + 1]);
+    yScale.domain([
+      //  just hardcode when using custom ration
+      0,
+      100,
+      //  min(flattenedData, yValue),
+      //  max(flattenedData, yValue),
+    ]);
+    sizeScale.domain([
+      min(flattenedData, sizeValue) - 1,
+      max(flattenedData, sizeValue) + 1,
+    ]);
+
+    const lineFn = line()
+      .x(xMap)
+      .y(yMap);
+
+    return {
+      xMap,
+      yMap,
+      lineFn,
+    };
+  }
+
   constructor(props) {
     super(props);
 
     this.chart = null;
     this.svg = null;
-  }
-
-  componentDidMount() {
-    this.updateChart();
-  }
-
-  componentDidUpdate() {
-    this.updateChart();
   }
 
   updateChart() {
@@ -170,7 +235,63 @@ class Chart extends Component {
     dots.exit().remove();
   }
 
+  renderPath(task, i) {
+    const { lineFn } = this.state;
+    const pathString = lineFn(task.toJS());
+
+    const stroke = COLORS[i];
+    console.log('stroke', stroke);
+
+    return (
+      <path
+        className={classes.progress}
+        style={{ stroke }}
+        d={pathString}
+      />
+    );
+  }
+
+  renderPaths() {
+    const { tasks } = this.props;
+
+    return (
+      <g>
+        {tasks.map(this.renderPath.bind(this))}
+      </g>
+    );
+  }
+
+  renderThumbnail(t, i) {
+    const { xMap, yMap } = this.state;
+    const thumbUrl = getVideoThumbnail(t.get('url'));
+
+    const tObj = t.toJS();
+    const left = xMap(tObj, i);
+    const top = yMap(tObj);
+    const style = { left, top };
+
+    return (
+      <img
+        key={i}
+        src={thumbUrl}
+        className={classes.thumb}
+        style={style}
+        alt="thumb"
+      />
+    );
+  }
+
+  renderThumbnails(tasks, i) {
+    return (
+      <div key={i} className={classes.thumbs}>
+        {tasks.map(this.renderThumbnail.bind(this))}
+      </div>
+    );
+  }
+
   render() {
+    const { tasks } = this.props;
+
     const transformString = `translate(${MARGIN.top}px,${MARGIN.left}px)`;
     const style = { transform: transformString };
 
@@ -180,11 +301,11 @@ class Chart extends Component {
         className={classes.chart}
       >
         <svg className={classes.svg}>
-          <g
-            ref={(el) => this.svg = el}
-            style={style}
-          />
+          {tasks.map(this.renderPaths.bind(this))}
         </svg>
+        <div>
+          {tasks.map(this.renderThumbnails.bind(this))}
+        </div>
         <div className={classes.tooltip} ref={(el) => this.tooltip = el} />
       </div>
     );
