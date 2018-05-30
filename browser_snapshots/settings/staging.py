69b9af321e0616cade2dccebbfeb915fe0fb0161
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-05-30 05:05:51 +0100

    Not gitignored

--- browser_snapshots/settings/staging.py
+++ browser_snapshots/settings/staging.py
@@ -23,7 +23,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'viz/static'),
 )
 
 
