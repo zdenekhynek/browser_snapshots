6663beb12ddea8d1b194ad30778731a330f176c2
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-06-03 18:19:10 +0100

    Results

--- frontend/src/ipad/results.jsx
+++ frontend/src/ipad/results.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Map } from 'immutable';
@@ -8,32 +8,61 @@ import { sendSocketMessage } from '../sockets/socket_service';
 import classes from './results.css';
 import landingClasses from './landing.css';
 
-const Results = ({ race }) => {
-  const keyword = (race && race.get('keyword'))?
-    race.get('keyword') : 'something';
-  const label = `Try searching for "${keyword}" on your YouTube.`;
+//  5 minute timeout interval
+export const TIMEOUT = 5 * 60 * 1000;
 
-  return (
-    <div className={classes.ipadResults}>
-      <div>
-        <h1 className={landingClasses.title}>
-          {label}
-        </h1>
-        <h2>
-          To find out, what’s next up for you.
-        </h2>
+export class Results extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onRestartClick = this.onRestartClick.bind(this);
+    this.restartTimeout = null;
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.restartTimeout = setTimeout(() => {
+      this.onRestartClick();
+    }, TIMEOUT);
+  }
+
+  componentDidUnmount() {
+    console.log('componentDidUnmount');
+    clearTimeout(restartTimeout);
+  }
+
+  onRestartClick() {
+    console.log('onRestartClick');
+    sendSocketMessage('restart');
+  }
+
+  render() {
+    const { race } = this.props;
+
+    const keyword = (race && race.get('keyword'))?
+      race.get('keyword') : 'something';
+    const label = `Try searching for "${keyword}" on your YouTube.`;
+
+    return (
+      <div className={classes.ipadResults}>
+        <div>
+          <h1 className={landingClasses.title}>
+            {label}
+          </h1>
+          <h2>
+            To find out, what’s next up for you.
+          </h2>
+        </div>
+        <button
+          className={landingClasses.link}
+          onClick={this.onRestartClick}
+        >
+          Start over
+        </button>
       </div>
-      <button
-        className={landingClasses.link}
-        onClick={() => {
-          sendSocketMessage('restart');
-        }}
-      >
-        Start over
-      </button>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Results.propTypes = {
   className: PropTypes.string,
