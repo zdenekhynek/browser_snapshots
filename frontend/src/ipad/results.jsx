054e19e75f9343ab308b54d54a4aa71a74e060ff
Zdenek Hynek <zdenek@signal-noise.co.uk>
2018-06-03 14:53:56 +0100

    Results page

--- frontend/src/ipad/results.jsx
+++ frontend/src/ipad/results.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Map } from 'immutable';
 
 import { sendSocketMessage } from '../sockets/socket_service';
 
 import classes from './results.css';
+import landingClasses from './landing.css';
 
-const Results = ({ className }) => {
+const Results = ({ race }) => {
+  const keyword = (race && race.get('keyword'))?
+    race.get('keyword') : 'something';
+  const label = `Try searching for "${keyword}" on your YouTube.`;
+
   return (
     <div className={classes.ipadResults}>
-      <h1>Ready for another round?</h1>
+      <div>
+        <h1 className={landingClasses.title}>
+          {label}
+        </h1>
+        <h2>
+          To find out, what’s up next for you.
+        </h2>
+      </div>
       <button
-        className={classes.btn}
+        className={landingClasses.link}
         onClick={() => {
           sendSocketMessage('restart');
         }}
       >
-        Yes, please!
+        Start over
       </button>
     </div>
   );
@@ -29,4 +43,17 @@ Results.defaultProps = {
   className: '',
 };
 
-export default Results;
+
+export function mapStateToProps({ agents, metrics, races }, { raceId }) {
+  const activeRace = races.find((r) => r.get('id', '') === +raceId, null, Map());
+
+  return {
+    race: activeRace,
+  };
+}
+
+Results.propTypes = {};
+
+Results.defaultProps = {};
+
+export default connect(mapStateToProps)(Results);
