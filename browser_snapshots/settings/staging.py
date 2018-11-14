5015bd886a1cadcb9b489fc4c007987caccda48d
Zdenek Hynek <zdenek.hynek@gmail.com>
2018-11-14 10:07:40 +0000

    Base dir

--- browser_snapshots/settings/staging.py
+++ browser_snapshots/settings/staging.py
@@ -75,7 +75,7 @@ DEBUG = True
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/',
-        'STATS_FILE': os.path.join(DATA_DIR, 'webpack-stats-staging.json'),
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-staging.json'),
     }
 }
 
