5105ed98f078121bb83ecc70e240775b21f4a6d4
Zdenek Hynek <zdenek.hynek@gmail.com>
2018-11-15 09:46:03 +0000

    Disable statics

--- browser_snapshots/settings/staging.py
+++ browser_snapshots/settings/staging.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(ROOT_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(ROOT_DIR, 'static'),
+# )
 
 
 MIDDLEWARE = [
